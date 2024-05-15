b6436819f717bf2557849b4aeecea73642a9372c
nits830 <nitishgupta830@gmail.com>
2024-05-16 00:37:33 +0530

    formatter.ts

---
+++ src/lib/formatter.ts
@@ -0,0 +1,24 @@
+function formatCurrency(amount: number): string {
+  const formattedAmount = amount.toFixed(2);
+  const [rupees, paisa] = formattedAmount.split(".");
+  const formattedRupees = rupees.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return `₹ ${formattedRupees}.${paisa}`;
+}
+
+function formatNumber(number: number): string {
+  const [integerPart, decimalPart] = number.toFixed(2).split(".");
+  const formattedIntegerPart = integerPart.replace(
+    /\B(?=(\d{3})+(?!\d))/g,
+    ","
+  );
+  const formattedNumber = decimalPart
+    ? `${formattedIntegerPart}.${decimalPart}`
+    : formattedIntegerPart;
+
+  return formattedNumber;
+}
+
+export default {
+  formatCurrency: formatCurrency,
+  formatNumber: formatNumber,
+};
