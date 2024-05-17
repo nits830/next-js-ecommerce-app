671b48f5dee7ed4ffc4c86e9f039e06987ec6550
NITISH GUPTA <31302317+nits830@users.noreply.github.com>
2024-05-17 12:02:12 +0000

    .

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,13 +1,15 @@
 "use client"
 
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
+import { formatCurrency } from '@/lib/formatter'
 import React, { useState } from 'react'
 
 const ProductForm = () => {
 
-    const [price, setPrice] = useState<number>()
+    const [price, setPrice] = useState<number>(0)
   return (
    <form action="" className='space-y-8'>
     <div className='space-y-2'>
@@ -16,13 +18,25 @@ const ProductForm = () => {
     </div>
     <div className='space-y-2'>
         <Label htmlFor='price'>Price</Label>
-        <Input type='text' id='price' name='price' required value={price} onChange={(e) => setPrice(Number(e.target.value) || undefined)}/>
+        <Input type='text' id='price' name='price' required value={price} onChange={e => setPrice(Number(e.target.value) || undefined)}/>
+    </div>
+    <div className='text-muted-foreground'>
+        {formatCurrency(price || 0)}
     </div>
 
     <div className='space-y-2'>
         <Label htmlFor='description'>Description</Label>
         <Textarea  id='description' name='description'/>
     </div>
+    <div className='space-y-2 '>
+        <Label htmlFor='file'>File</Label>
+        <Input type='file' id='file' name='file' required/>
+    </div>
+    <div className='space-y-2 '>
+        <Label htmlFor='image'>Image</Label>
+        <Input type='file' id='image' name='image' required/>
+    </div>
+    <Button type='submit'>Save</Button>
        
    </form>
   )
